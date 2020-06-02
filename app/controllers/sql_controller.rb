c22d2236c6c1a16266c7a9a7ba53339dcd19169f
AntonDyrdin <dyrdin.a.i@yandex.ru>
2020-06-02 21:53:07 +0300

    фикс багов

--- app/controllers/sql_controller.rb
+++ app/controllers/sql_controller.rb
@@ -2,10 +2,14 @@ class SqlController < ActionController::API
     
     def execute_sql
         begin
-            response = ActiveRecord::Base.connection.exec_query(params['query']).to_a
+            response = ActiveRecord::Base.connection.exec_query(params['query'].gsub('**point**', '.')).to_a
             response = JSON.pretty_generate( response )
         rescue => exception
             response = exception.message + '\n' + exception.backtrace[0] 
+        end
+
+        if response == "[\n\n]"
+            response = params['query'].gsub('**point**', '.') + "\n\n"
         end
         render plain: response
     end
